eb3091d1b67212ea009b61ae4a8cf2876869201f
Max Miguta <max_miguta@yahoo.com>
2018-11-08 12:34:01 +0000

    Allow connection to remote DB

--- settings/staging.py
+++ settings/staging.py
@@ -2,7 +2,7 @@ from base import *
 import dj_database_url
 
 DEBUG = False
-
+"""
 # Local Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
@@ -15,7 +15,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+"""
 # Load the ClearDB connection details from the environment variable
 DATABASES = {
     'default': dj_database_url.config('CLEARDB_DATABASE_URL')
