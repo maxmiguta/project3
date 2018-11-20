5183385a39c16e245698ebf6ad8fee0fcbf380af
Max Miguta <max_miguta@yahoo.com>
2018-11-20 03:23:09 +0000

    Amended email backend details

--- settings/staging.py
+++ settings/staging.py
@@ -31,7 +31,6 @@ EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
 
