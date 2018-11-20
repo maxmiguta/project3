7ea1ab2e683d034119159e6988972a8e8ba477da
Max Miguta <max_miguta@yahoo.com>
2018-11-20 03:07:23 +0000

    Amended email backend details

--- settings/staging.py
+++ settings/staging.py
@@ -27,12 +27,13 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 # Use Django's email backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 587
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 
 SITE_URL = 'https://av-empire.herokuapp.com'
 ALLOWED_HOSTS.append('av-empire.herokuapp.com')
