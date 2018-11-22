3cf1d8613416f5c070977938d13fff6e02a32ff4
Max Miguta <max_miguta@yahoo.com>
2018-11-22 15:11:38 +0000

    Added phone number to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -5,5 +5,5 @@ from phonenumber_field.formfields import PhoneNumberField
 class ContactForm(forms.Form):
     contact_name = forms.CharField(label='Full name', widget=forms.TextInput({'placeholder': 'Enter your full name'}), required=True)
     contact_email = forms.EmailField(label='Email', widget=forms.TextInput({'placeholder': 'Enter your email'}), required=True)
-    phone_number = PhoneNumberField(label='Phone number', widget=forms.TextInput({'placeholder': 'Enter your number'}))
+    phone_number = PhoneNumberField(label='Phone number', widget=forms.TextInput({'placeholder': 'Enter your number'}), required=False)
     message = forms.CharField(label='Your enquiry', widget=forms.Textarea({'placeholder': 'Enter your message'}), required=True)
