c00bf354932acebeba17ea63d586ba4b98f41dd5
Max Miguta <max_miguta@yahoo.com>
2018-12-06 20:50:06 +0000

    Updated phone_number field

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(label='Full name', widget=forms.TextInput({'placeholder': 'Enter your full name'}), required=True)
     contact_email = forms.EmailField(label='Email', widget=forms.TextInput({'placeholder': 'Enter your email'}), required=True)
-    phone_number = PhoneNumberField(label='Phone number', widget=forms.TextInput({'placeholder': 'Enter a number beginning with your area code'}), required=False)
+    phone_number = forms.CharField(validators=[RegexValidator(r'^\d{11}$')], error_messages={'invalid': 'Does not meet requirement'}, required=False, strip=True)
     message = forms.CharField(label='Your enquiry', widget=forms.Textarea({'placeholder': 'Enter your message'}), required=True)
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    phone_regex = RegexValidator(regex=r'^\d{11}$', message="Phone number must have 11 digits.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
