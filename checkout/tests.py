868c0f6677933319fdb952313912606dadd6b999
Max Miguta <max_miguta@yahoo.com>
2018-12-14 15:30:12 +0000

    Added automated tests

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,6 +1,48 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from .forms import OrderForm
+from django import forms
 
-# Create your tests here.
+
+class CustomOrderTest(TestCase):
+
+    # Test to check that validation works when a user doesn't provide their full name
+    def test_order_form_fails_with_missing_full_name(self):
+        order_form = OrderForm({
+            'phone_number': '01234567890',
+            'street_address1': 'address',
+            'town_or_city': 'city',
+            'postcode': 'ab1 2cd',
+            'country': 'country',
+        })
+        self.assertFalse(order_form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Please enter your name",
+                                 order_form.full_clean())
+
+    # Test to check that validation works when a user doesn't provide their phone number
+    def test_order_form_fails_with_missing_phone_number(self):
+        order_form = OrderForm({
+            'full_name': 'full name',
+            'street_address1': 'address',
+            'town_or_city': 'city',
+            'postcode': 'ab1 2cd',
+            'country': 'country',
+        })
+        self.assertFalse(order_form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Please enter your phone number",
+                                 order_form.full_clean())
+
+    # Test to check that validation works when a user doesn't provide their address
+    def test_order_form_fails_with_missing_address(self):
+        order_form = OrderForm({
+            'full_name': 'full name',
+            'phone_number': '01234567890',
+            'town_or_city': 'city',
+            'postcode': 'ab1 2cd',
+            'country': 'country',
+        })
+        self.assertFalse(order_form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Please enter your address",
+                                 order_form.full_clean())
--- products/tests.py
+++ products/tests.py
@@ -1,14 +1,6 @@
-from django.test import TestCase
-from .models import Product
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.test import TestCase
 
 # Create your tests here.
-class ProductTests(TestCase):
-    """
-    Here we'll define the tests
-    that we'll run against our Product models
-    """
-
-    def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
